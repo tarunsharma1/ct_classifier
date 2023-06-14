78058d28dd370afb41dcaeaa99d7f72b1481e827
tarun <tarunsuper@gmail.com>
2023-06-14 13:45:02 -0700

    modify data reading for custom dataset

--- ct_classifier/dataset.py
+++ ct_classifier/dataset.py
@@ -17,6 +17,7 @@ import json
 from torch.utils.data import Dataset
 from torchvision.transforms import Compose, Resize, ToTensor
 from PIL import Image
+import csv
 
 
 class CTDataset(Dataset):
@@ -36,30 +37,52 @@ class CTDataset(Dataset):
         # index data into list
         self.data = []
 
-        # load annotation file
-        annoPath = os.path.join(
-            self.data_root,
-            'eccv_18_annotation_files',
-            'train_annotations.json' if self.split=='train' else 'cis_val_annotations.json'
-        )
-        meta = json.load(open(annoPath, 'r'))
+        self.label_mapping = {}
+        global_mapping_idx = 0
 
-        images = dict([[i['id'], i['file_name']] for i in meta['images']])          # image id to filename lookup
-        labels = dict([[c['id'], idx] for idx, c in enumerate(meta['categories'])]) # custom labelclass indices that start at zero
-        
-        # since we're doing classification, we're just taking the first annotation per image and drop the rest
-        images_covered = set()      # all those images for which we have already assigned a label
-        for anno in meta['annotations']:
-            imgID = anno['image_id']
-            if imgID in images_covered:
-                continue
+        if split == 'train':
+            f = open(cfg['train_label_file'], 'r')
+        else:
+            f = open(cfg['val_label_file'], 'r')
+
+        csv_reader = csv.reader(f, delimiter=',')
+        for row in csv_reader:
+            if int(row[1]) not in list(self.label_mapping.keys()):
+                self.label_mapping[int(row[1])] = global_mapping_idx
+                global_mapping_idx += 1
+
             
-            # append image-label tuple to data
-            imgFileName = images[imgID]
-            label = anno['category_id']
-            labelIndex = labels[label]
-            self.data.append([imgFileName, labelIndex])
-            images_covered.add(imgID)       # make sure image is only added once to dataset
+            self.data.append([row[0], self.label_mapping[int(row[1])]])
+
+
+            ### over here do a mapping for labels from their actual label to a number from 0 to num_classes-1
+
+
+
+        # load annotation file
+        # annoPath = os.path.join(
+        #     self.data_root,
+        #     'eccv_18_annotation_files',
+        #     'train_annotations.json' if self.split=='train' else 'cis_val_annotations.json'
+        # )
+        # meta = json.load(open(annoPath, 'r'))
+
+        # images = dict([[i['id'], i['file_name']] for i in meta['images']])          # image id to filename lookup
+        # labels = dict([[c['id'], idx] for idx, c in enumerate(meta['categories'])]) # custom labelclass indices that start at zero
+        
+        # # since we're doing classification, we're just taking the first annotation per image and drop the rest
+        # images_covered = set()      # all those images for which we have already assigned a label
+        # for anno in meta['annotations']:
+        #     imgID = anno['image_id']
+        #     if imgID in images_covered:
+        #         continue
+            
+        #     # append image-label tuple to data
+        #     imgFileName = images[imgID]
+        #     label = anno['category_id']
+        #     labelIndex = labels[label]
+        #     self.data.append([imgFileName, labelIndex])
+        #     images_covered.add(imgID)       # make sure image is only added once to dataset
     
 
     def __len__(self):
@@ -77,7 +100,7 @@ class CTDataset(Dataset):
         image_name, label = self.data[idx]              # see line 57 above where we added these two items to the self.data list
 
         # load image
-        image_path = os.path.join(self.data_root, 'eccv_18_all_images_sm', image_name)
+        image_path = os.path.join(self.data_root, image_name)
         img = Image.open(image_path).convert('RGB')     # the ".convert" makes sure we always get three bands in Red, Green, Blue order
 
         # transform: see lines 31ff above where we define our transformations
